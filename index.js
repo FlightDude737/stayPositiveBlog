9102bbc262e329953dc3e9a593b7123aad1b7bbc
FlightDude737 <andrewdc@madisoncity.k12.al.us>
2020-04-03 18:48:48 +0000

    Almost complete with user authenication. Sign up is functional and login is close.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const fs = require("fs")
 const express = require("express")
+const request = require("request")
+
+let passwordHash = ""
+let usernameSet = "User"
+const dataBuffer = fs.readFileSync("info.json")
+
+if (JSON.parse(fs.readFileSync('info.json').toString() == "")) {
+  fs.writeFileSync('info.json', "[]")
+}
 
 const app = express()
 
@@ -8,11 +17,25 @@ app.set("views", "public")
 app.use(express.static("/public"))
 
 app.get('/', (req, res) => {
-  res.render('index.hbs')
+  res.render('index.hbs', {
+    username: usernameSet
+  })
 })
 
 app.get('/login', (req, res) => {
   res.render('login.hbs')
+})
+
+app.get('/login/authenticate', (req, res) => {
+  if (!req.query.username || !req.query.password) {
+    res.send("There is no username or password. Please input your credidentials.")
+  } else {
+    console.log(req.query.password)
+  }
+})
+
+app.get('/sign-up', (req, res) => {
+  res.render('signup.hbs')
 })
 
 app.get('/users', (req, res) => {
@@ -21,13 +44,32 @@ app.get('/users', (req, res) => {
       error: "No value was provided."
     })
   } else {
-    let cred = {
-      username: req.query.username,
-      password: req.query.password
+    var url = 'https://api.hashify.net/hash/sha512/hex?value=' + req.query.password
+    request( { url, json: true }, function (error, response) { 
+      if ( error ) {
+        console.log(error)
+      }
+      passwordHash = response.body.Digest
+    })
+
+    try{
+        const dataBuffer = fs.readFileSync('info.json')
+        const dataJSON = dataBuffer.toString()
+        const info = JSON.parse(dataJSON)
+        info.push({
+          username: req.query.username,
+          password: passwordHash
+        })
+        const finalData = JSON.stringify(info)
+        fs.writeFileSync('info.json', finalData)
+    } catch (e) {
+        return []
     }
-    const accountData = JSON.stringify(cred)
-    fs.appendFileSync('info.txt', accountData)
-    res.send(accountData)
+
+    
+    usernameSet = req.query.username
+    res.send("<html><script>window.location = 'https://staypositiveblog--flightdude737.repl.co/';</script></html>")
+    console.log(JSON.parse(fs.readFileSync('info.json')))
   }
 })
 
@@ -38,13 +80,3 @@ app.get('*', (req, res) => {
 app.listen(3000, () => {
   console.log("The server is up on port 3000.")
 })
-let username = "hello"
-let password = "hello"
-
-const jsonDataObject = {
-  username: username,
-  password: password
-}
-
-const jsonData = JSON.stringify(jsonDataObject)
-fs.writeFileSync("info.txt", jsonData)
---
+++ public/script.js
@@ -0,0 +1,3 @@
+function createAccount(firstName, lastName, email, username, password){
+  window.location = "https://staypositiveblog--flightdude737.repl.co/users?username=" + username + "&password=" + password;
+}
